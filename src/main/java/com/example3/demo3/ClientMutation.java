267f344ec97a6a6ad98fdf1bd25e9ed601f6657c
Alex <alexander.stepaniuk@yahoo.com.ua>
2020-02-21 17:50:48 +0200

    com.graphql-java-kickstart added

--- src/main/java/com/example3/demo3/ClientMutation.java
+++ src/main/java/com/example3/demo3/ClientMutation.java
@@ -1,6 +1,7 @@
 package com.example3.demo3;
 
 
+import graphql.kickstart.tools.GraphQLMutationResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
